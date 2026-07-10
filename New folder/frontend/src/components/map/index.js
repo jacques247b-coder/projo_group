5fd19b4226dffac1cbb123cb367e9e66139f3470
jacques247b-coder <jacques247b@gmail.com>
2026-07-10 16:42:38 +0200

    Fix PWA install icon path, remove old orphaned project folder

--- New folder/frontend/src/components/map/index.js
+++
@@ -1,6 +0,0 @@
-// PROJO GROUP — Map components barrel export
-export { default as ProjoMap } from "./ProjoMap";
-export { default as BookingMap } from "./BookingMap";
-export { default as LiveTrackingMap } from "./LiveTrackingMap";
-export { default as AdminLiveMap } from "./AdminLiveMap";
-export { goldIcon, pickupIcon, dropoffIcon, driverIcon } from "./ProjoMap";
